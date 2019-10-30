1322a90f6768df69da6db3d1e5c1b87ddb5bd22f
naveen1111 <ngnaven@gmail.com>
2019-10-30 17:27:47 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const app = express();
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'naveen',
+        password : 'naveen',
+        database : 'smart-brain'
+    }
+  });
+
+app.use(bodyParser.json());
+app.use(cors());
+app.get('/', (req, res) => {res.send(database.users);})
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.put('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+app.listen(3000, ()=> {console.log('app is running on port 3000');})
---
+++ server2.js
@@ -0,0 +1,10 @@
+const app = require('http')
+    .createServer((req, res) => res.send('oh hi there!'));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
+
+console.log(PORT);
