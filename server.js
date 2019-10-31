2afcbbf85040bda00707958b33a157501dc56650
naveen1111 <ngnaven@gmail.com>
2019-10-31 12:19:24 +0530

    adding connections

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'postgresql-shallow-48660',
-        user : 'naveen',
-        password : 'naveen',
-        database : 'smart-brain'
+        host : process.env.DATABASE_URL,
+        ssl : true,
     }
   });
 
