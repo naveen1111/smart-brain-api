810ff95b3e1328f8b4582625ad463926e9a305ef
naveen1111 <ngnaven@gmail.com>
2019-11-14 19:59:27 +0530

    checking API eror

--- controllers/image.js
+++ controllers/image.js
@@ -8,9 +8,10 @@ const handleApiCall = (req, res) => {
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'))
+    .catch(err => console.log(err))
 };
 
+// .catch(err => res.status(400).json('unable to work with API'))
 
 const handleImage = (req, res, db) => {
     const {id} = req.body;
